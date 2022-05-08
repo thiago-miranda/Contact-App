154ef2abcd19f8b4e0d07a9d58ef3c5222895cd0
thiago-miranda <thiago.miranda@hotmail.com>
2022-05-08 13:14:22 -0300

    edicao de contato

--- lib/ios/views/editor-contact.view.dart
+++ lib/ios/views/editor-contact.view.dart
@@ -9,7 +9,46 @@ class EditorContactView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Text("L"),
+      child: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(
+            largeTitle: model == null
+                ? const Text("Novo Contato")
+                : const Text("Editar Contato"),
+          ),
+          SliverFillRemaining(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    CupertinoTextField(
+                      placeholder: model?.name ?? "Nome",
+                    ),
+                    const SizedBox(height: 20),
+                    CupertinoTextField(
+                      placeholder: model?.phone ?? "Telefone",
+                    ),
+                    const SizedBox(height: 20),
+                    CupertinoTextField(
+                      placeholder: model?.email ?? "Email",
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: CupertinoButton.filled(
+                        child: const Text("Salvar"),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
