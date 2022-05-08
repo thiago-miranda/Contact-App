08db7b22ca03b7bb8290c42f4d96e1c15ff10a54
thiago-miranda <thiago.miranda@hotmail.com>
2022-05-07 23:29:29 -0300

    iosHome

--- lib/ios/views/home.view.dart
+++ lib/ios/views/home.view.dart
@@ -1,3 +1,4 @@
+import 'package:contact_app/ios/styles.dart';
 import 'package:flutter/cupertino.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,12 +6,93 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
+    return CupertinoPageScaffold(
       child: CustomScrollView(
         slivers: [
           CupertinoSliverNavigationBar(
-            largeTitle: Text("Meus Contatos"),
-          )
+            largeTitle: const Text("Meus Contatos"),
+            trailing: CupertinoButton(
+              child: const Icon(
+                CupertinoIcons.add,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (context) => const HomeView(),
+                  ),
+                );
+              },
+            ),
+          ),
+          SliverFillRemaining(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CupertinoTextField(
+                    placeholder: "Pesquisar...",
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: 48,
+                              width: 48,
+                              decoration: BoxDecoration(
+                                image: const DecorationImage(
+                                  image: NetworkImage(
+                                    "http://images6.fanpop.com/image/photos/39900000/IMG-6250-PNG-kion-39961687-1024-577.png",
+                                  ),
+                                ),
+                                borderRadius: BorderRadius.circular(48),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      "Sinon",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    Text(
+                                      "11 99999 0000",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            CupertinoButton(
+                              child: const Icon(
+                                CupertinoIcons.person,
+                                color: primaryColor,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
